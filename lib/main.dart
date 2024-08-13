ac57a98f94c696d2a69327f075d9be4ee6c7cd10
jypark <jyparkr@gmail.com>
2024-08-13 22:36:41 +0900

    chapter 2.1 Header - Color - Padding - SizedBox - Column - Row - Children(Child) - Alignment(main:row, cross:column) - TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,46 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello flutter!'),
-        ),
-        body: const Center(
-          child: Text('Hello world!'),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
-  
 }
