c0b91e61656872bfa66bf9c06b526914d0411619
jypark <jyparkr@gmail.com>
2024-08-18 20:19:20 +0900

    chapter 3.2 BuildContext - approach topper property

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ class App extends StatefulWidget {
 // just dart class property
 class _AppState extends State<App> {
   // int counter = 0;
-  List<int> numbers = [];
+  // List<int> numbers = [];
 
+  /*
   void onClicked() {
     /* method 1 (recommended)
     setState(() {
@@ -26,44 +27,70 @@ class _AppState extends State<App> {
     /* method 2
     counter = counter + 1;
     setState(() {});
-    */
-
+    
     setState(() {
       numbers.add(numbers.length);
     });
+    */
   }
+  */
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              /*
               const Text(
                 'Click Count',
                 style: TextStyle(
                   fontSize: 30,
                 ),
               ),
-              /*
               Text(
                 '$counter',
                 style: const TextStyle(
                   fontSize: 30,
                 ),
               ),
-              */
               for (var n in numbers) Text('$n'),
               IconButton(
                   iconSize: 40,
                   onPressed: onClicked,
-                  icon: const Icon(Icons.add_box_rounded)),
+                  icon: const Icon(Icons.add_box_rounded)),                  
+               */
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
