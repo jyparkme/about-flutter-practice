ad5cbe7bb56f2bc1c5dad2f0f5d54c242b61d469
jypark <jypark@kmi.or.kr>
2024-08-21 07:42:43 +0900

    chapter 5.4 FutureBuilder - future(function) / builder - context, snapshot(status)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -10,9 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(),
-      ),
+      home: HomeScreen(),
     );
   }
 }
